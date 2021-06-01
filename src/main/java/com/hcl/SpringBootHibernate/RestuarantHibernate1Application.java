7d30de9f02a684e44fa07711b2bd03bc5c8bfe08
BiraganiTejeswini <84894642+BiraganiTejeswini@users.noreply.github.com>
2021-06-01 11:44:31 +0530

    Update RestuarantHibernate1Application.java

--- src/main/java/com/hcl/SpringBootHibernate/RestuarantHibernate1Application.java
+++ src/main/java/com/hcl/SpringBootHibernate/RestuarantHibernate1Application.java
@@ -2,12 +2,16 @@ package com.hcl.SpringBootHibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; 
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class RestuarantHibernate1Application {
+public class RestuarantHibernate1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestuarantHibernate1Application.class, args);
 	}
+	@Override     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {        
+		return application.sources(RestuarantHibernate1Application.class);     }
 
 }
